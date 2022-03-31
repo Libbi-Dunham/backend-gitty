7c0ed928f4394bb7f03ec891443b1bfdc7eea81a
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-03-31 15:50:04 -0700

    wrote test and controller for signing out a user

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const GithubUser = require('../lib/models/GithubUser');
 
 jest.mock('../lib/utils/github');
 
@@ -33,6 +34,23 @@ describe('backend-gitty routes', () => {
       email: 'not-real@example.com',
       iat: expect.any(Number),
       exp: expect.any(Number),
+    });
+  });
+
+  it('should sign out a user', async () => {
+    const agent = request.agent(app);
+    await GithubUser.insert({
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+    });
+    await agent
+      .post('/api/v1/github/login')
+      .send({ username: 'fake_github_user', email: 'not-real@example.com' });
+
+    const res = await agent.delete('/api/v1/github/login');
+    expect(res.body).toEqual({
+      success: true,
+      message: 'Successfully signed out',
     });
   });
 });
