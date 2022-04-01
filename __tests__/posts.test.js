0bd7313da1e3304140e2327aab170f649ab430b2
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-04-01 14:22:33 -0700

    wrote out test and model for being able to list all posts

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/github');
+
 describe('backend-gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +12,25 @@ describe('backend-gitty routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should be able to list all posts', async () => {
+    const agent = request.agent(app);
+    await request(app).get('/api/v1/github/login');
+    await agent.get('/api/v1/github/login/callback?code=42').redirects(1);
+
+    const firstPost = {
+      id: expect.any(String),
+      title: 'first post',
+      content: 'hello',
+    };
+
+    const secondPost = {
+      id: expect.any(String),
+      title: 'second post',
+      content: 'bye',
+    };
+    const res = await agent.get('/api/v1/posts');
+    expect(res.body).toEqual([firstPost, secondPost]);
   });
 });
